7e158e4e25f6003e1067a45e7c11312b5659ac98
YASH2204V <yash.v220104@gmail.com>
2024-11-09 11:06:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+})
