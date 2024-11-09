b570953ccd37d1a45062f1b2bc1145bba7db4200
YASH2204V <yash.v220104@gmail.com>
2024-11-09 15:26:18 +0530

    #6

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-const cspMiddleware = require('./middleware/csp');
-app.use(cspMiddleware);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 app.get("/", (req,res)=>{
     res.render("index");
 })
+
+// app.use((req, res, next) => {
+//     res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
+//     next();
+//   });
 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`);
